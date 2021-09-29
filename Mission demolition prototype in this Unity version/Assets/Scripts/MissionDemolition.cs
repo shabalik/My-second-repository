f9672aa1879942496270bec81f17e3f6f2e5324b
shabalik <shabalshabal1@gmail.com>
2021-09-29 08:31:06 +0300

    1st commit
    lets try to push

---
+++ Mission demolition prototype in this Unity version/Assets/Scripts/MissionDemolition.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+	idle,
+	playing,
+	levelEnd
+}
+
+public class MissionDemolition : MonoBehaviour {
+
+	static private MissionDemolition S;
+
+	[Header("Set in Inspector")]
+	public Text uitLevel;
+	public Text uitShots;
+	public Text uitButton;
+	public Vector3 castlePos;
+	public GameObject[] castles;
+
+	[Header("Set dynamically")]
+	public int level;
+	public int levelMax;
+	public int shotsTaken;
+	public GameObject castle;
+	public GameMode mode = GameMode.idle;
+	public string showing = "Show Slingshot";
+
+	// Use this for initialization
+	void Start () {
+		S = this;
+		level = 0;
+		levelMax = castles.Length;
+		StartLevel();
+	}
+
+	void StartLevel()
+    {
+		if (castle != null)
+        {
+			Destroy(castle);
+        }
+		GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach (GameObject item in gos)
+        {
+			Destroy(item);
+        }
+		castle = Instantiate<GameObject>(castles[level]);
+		castle.transform.position = castlePos;
+		shotsTaken = 0;
+
+		SwitchView("Show Both");
+		ProjectileLine.S.Clear();
+
+		Goal.goalMet = false;
+
+		UpdateGUI();
+
+		mode = GameMode.playing;
+    }
+
+	void UpdateGUI()
+    {
+		uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
+		uitShots.text = "Shots taken: " + shotsTaken;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateGUI();
+		if ((mode == GameMode.playing) && Goal.goalMet)
+        {
+			mode = GameMode.levelEnd;
+			SwitchView("Show Both");
+			Invoke("NextLevel", 2f);
+        }
+		
+	}
+
+	void NextLevel()
+    {
+		level++;
+		if (level == levelMax)
+        {
+			level = 0;
+        }
+		StartLevel();
+    }
+
+	public void SwitchView(string eView = "")
+    {
+		if (eView == "")
+        {
+			eView = uitButton.text;
+        }
+		showing = eView;
+		switch (showing)
+        {
+			case "Show Slingshot":
+				FollowCam.POI = null;
+				uitButton.text = "Show castle";
+				break;
+			case "Show castle":
+				FollowCam.POI = S.castle;
+				uitButton.text = "Show Both";
+				break;
+			case "Show Both":
+				FollowCam.POI = GameObject.Find("ViewBoth");
+				uitButton.text = "Show Slingshot";
+				break;
+        }
+    }
+
+	public static void ShotFired()
+    {
+		S.shotsTaken++;
+    }
+}
